709c413f15375d8704a972bc50b2921c68d51049
João Vitor Dutra <jo.vitor.d@gmail.com>
2020-06-02 08:32:38 -0300

    Hello World server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,8 +2,9 @@ import express from "express";
 
 const app = express();
 
-app.get("/users", () => {
+app.get("/users", (request, response) => {
   console.log("Listagem de Usuários");
+  response.send("Hello World");
 });
 
 app.listen(3333);
