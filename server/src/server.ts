9dc5c5b1601c3e987aa929223ae22286c3a1488e
João Vitor Dutra <jo.vitor.d@gmail.com>
2020-06-02 08:46:14 -0300

    Update server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,7 +5,7 @@ const app = express();
 app.get("/users", (request, response) => {
   console.log("Listagem de Usuários");
   //JSON
-  response.json(["Diego", "Cleiton", "Robson", "João", "Mayk"]);
+  response.json(["Diego", "Cleiton", "Robson"]);
 });
 
 app.listen(3333);
